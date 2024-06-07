cbbd897b7322efc70078a5b28848ca196dfddfea
YuriiMarku <yurii.marku@infologiq.com>
2024-06-07 13:20:10 +0200

    Set up typescript configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
-import React from "react";
+import React, { FC, ReactElement } from "react";
 
-function App() {
-
-    return (
-        <div>Todo list</div>
-    );
-}
-
+const App: FC = (): ReactElement => (
+    <div>Todo list</div>
+);
 export default App;
