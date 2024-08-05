13b1c1188e1e9dc76c8b9d4d1ea63da2d8e1b0ce
YuriiMarku <yurii.marku@infologiq.com>
2024-08-06 01:40:20 +0200

    revert local ip

--- src/configs/serverConfig.tsx
+++ src/configs/serverConfig.tsx
@@ -1,7 +1,7 @@
 export default class ServerConfig{
     static devMode: boolean | null = true; // true for development; false for production; null for staging.
     static prodUrl: string = 'http://localhost:4000/';
-    static testUrl: string = 'http://192.168.55.4:4000/';
+    static testUrl: string = 'http://localhost:4000/';
     static stagingUrl: string = 'http://localhost:4000/';
 
     static getUrl(): string{
