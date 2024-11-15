cd89d575bc633a5bf55fd3d810d192b630e6dd77
YuriiMarku <yurii.marku@infologiq.com>
2024-11-15 02:33:27 +0100

    Changed logic of product change

--- src/components/pages/Homepage/listPage/EditProductModal.tsx
+++ src/components/pages/Homepage/listPage/EditProductModal.tsx
@@ -1,153 +1,172 @@
-import React, {useEffect, useState} from 'react';
-import ReactDOM from "react-dom";
-import {Button, Modal, Form, InputGroup, Col} from "react-bootstrap";
-import {updateProductsListRequest} from "../../../../actions/products";
-import {deleteProductFromList} from "../../../../actions/products";
-import {connect, useSelector} from "react-redux";
-import {MdShoppingCart} from "react-icons/md";
-import {FaPlusMinus} from "react-icons/fa6";
-import {useParams} from "react-router-dom";
-import {getCurrencySymbol, preventCharacters} from "../../../../helpers/helper";
-import { ProductCategories , ProductCategory} from "../../../../types/types";
+import React, { useCallback, useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
+import { Button, Modal, Form, InputGroup, Col } from 'react-bootstrap';
+import { updateProductsListRequest } from '../../../../actions/products';
+import { deleteProductFromList } from '../../../../actions/products';
+import { connect, useSelector } from 'react-redux';
+import { MdShoppingCart } from 'react-icons/md';
+import { FaPlusMinus } from 'react-icons/fa6';
+import { useParams } from 'react-router-dom';
+import { debounce, getCurrencySymbol, preventCharacters } from '../../../../helpers/helper';
+import { ProductCategories, ProductCategory } from '../../../../types/types';
 
-const EditProductModal = ({product, show, onHide, deleteProductFromList,updateProductsListRequest}) => {
+const EditProductModal = ({ product, show, onHide, deleteProductFromList, updateProductsListRequest }) => {
     const { listId } = useParams();
     const [name, setName] = useState('');
     const [count, setCount] = useState('1');
     const [price, setPrice] = useState('0');
     const [category, setCategory] = useState<ProductCategory | string>('');
     const country = useSelector(state => state.user.user.country);
+    const [debouncedValues, setDebouncedValues] = useState({ name, count, price });
 
     useEffect(() => {
         if (Object.keys(product).length) {
-            setName(product?.name ?? '');
+            setName(product.name);
             setCount(product?.count ? product.count : '1');
-            setPrice(product?.price ? product.price : '0');
+            setPrice(product?.price ? product.price : 0);
             setCategory(product?.category ?? '' as ProductCategory);
         }
     }, [product]);
 
-    const hasChanges = () => {
-        return product.name !== name ||
-            parseInt(product.count) !== count ||
-            product.price.toString() !== price ||
-            product.category !== category;
-    }
     const applyUpdate = () => {
         const prod = {
             _id: product._id,
             checked: product.checked,
-            name, count, price: parseFloat(price || '0'),
+            name, count,
+            price: parseFloat(price || '0'),
             category,
+        };
+        if (price.toString() && name && count.toString()) {
+            updateProductsListRequest(listId, [prod]);
         }
-        if(hasChanges() && price && name && count) {
-             updateProductsListRequest(listId, [prod]);
-        }
-    }
-    const deleteProduct = () => {
-        const prod = {
-            _id: product._id,
-        }
-        deleteProductFromList(listId, [prod]);
-        onHide();
-    }
+    };
 
-    return ReactDOM.createPortal(<Modal show={show} onHide={() => {
+    const updateListRequestDebounced = useCallback(
+        debounce((updatedValues) => {
+            setDebouncedValues(updatedValues);
+        }, 500),
+        []
+    );
+
+    useEffect(() => {
+        updateListRequestDebounced({ name, count, price });
+    }, [name, count, price, updateListRequestDebounced]);
+
+    useEffect(() => {
+        if (debouncedValues.name || debouncedValues.count || debouncedValues.price) {
+            if(product.name !== debouncedValues.name ||product.count !== debouncedValues.count ||product.price !== debouncedValues.price) {
+                applyUpdate();
+            }
+        }
+    }, [debouncedValues]);
+
+    useEffect(() => {
+        if (category !== product.category) {
+            applyUpdate();
+        }
+    }, [category]);
+
+    const deleteProduct = () => {
+        deleteProductFromList(listId, [{ _id: product._id }]);
         onHide();
-        if (price === '') {
-            setPrice(product.price);
-        }
-        if (count === '') {
-            setCount(product.count);
-        }
-        if (name === '') {
-            setCount(product.name);
-        }
-    }} className='w-100' centered>
-        <Modal.Header className='d-flex justify-content-center modal-styled-bg'>
-            <Modal.Title className='justify-content-center'>Product Editing</Modal.Title>
-        </Modal.Header>
-        <Modal.Body className='d-flex align-items-start flex-column modal-fixed-height modal-styled-bg'>
-            <Form.Group as={Col} className='mx-auto'>
-                <InputGroup className='my-2'>
-                    <Form.Label column sm="2" className='subtitle mx-2 w-25'>
-                        Name
-                    </Form.Label>
-                    <Form.Control
-                        type="text"
-                        onBlur={() => applyUpdate()}
-                        onChange={(e) => setName(e.target.value)}
-                        value={name}
-                    />
-                    <InputGroup.Text>
-                        <MdShoppingCart />
-                    </InputGroup.Text>
-                </InputGroup>
-                <InputGroup className='my-2'>
-                    <Form.Label column sm="2" className='subtitle mx-2 w-25'>
-                        Count
-                    </Form.Label>
-                    <Form.Control
-                        type="number"
-                        onBlur={() => applyUpdate()}
-                        min={1}
-                        onKeyPress={event => preventCharacters(event)}
-                        onChange={(e) => {
-                            setCount(e.target.value)
-                        }}
-                        value={count}
-                    />
-                    <InputGroup.Text>
-                        <FaPlusMinus />
-                    </InputGroup.Text>
-                </InputGroup>
-                <InputGroup className='my-2'>
-                    <Form.Label column sm="2" className='subtitle mx-2 w-25'>
-                        Price
-                    </Form.Label>
-                    <Form.Control
-                        type="number"
-                        inputMode="decimal"
-                        min={1}
-                        onBlur={() => applyUpdate()}
-                        onKeyPress={event => preventCharacters(event)}
-                        onChange={(e) => {
-                            setPrice(e.target.value)
-                        }}
-                        value={price}
-                    />
-                    <InputGroup.Text>
-                        <span>{getCurrencySymbol(country)}</span>
-                    </InputGroup.Text>
-                </InputGroup>
-                <InputGroup className='my-2'>
-                    <Form.Label column sm="2" className='subtitle mx-2 w-25'>
-                        Category
-                    </Form.Label>
-                    <Form.Select
-                        aria-label="gender"
-                        onBlur={() => applyUpdate()}
-                        onChange={(e) => setCategory(e.target.value) as ProductCategory}
-                        value={category}>
-                        {ProductCategories.map((category) => (
-                            <option key={category} value={category}>
-                                {category}
-                            </option>
-                        ))}
-                    </Form.Select>
-                </InputGroup>
-            </Form.Group>
-        </Modal.Body>
-        <Modal.Footer className='modal-styled-bg justify-content-around'>
-            <Button onClick={() => deleteProduct()}>Remove Product</Button>
-        </Modal.Footer>
-    </Modal>, document.body);
+    };
+
+    return ReactDOM.createPortal(
+        <Modal
+            show={show}
+            onHide={() => {
+                onHide();
+                if (price === '') setPrice(product.price);
+                if (count === '') setCount(product.count);
+                if (name === '') setCount(product.name);
+            }}
+            className='w-100'
+            centered
+        >
+            <Modal.Header className='d-flex justify-content-center modal-styled-bg'>
+                <Modal.Title className='justify-content-center title'>Product Editing</Modal.Title>
+            </Modal.Header>
+            <Modal.Body className='d-flex align-items-start flex-column modal-fixed-height modal-styled-bg'>
+                <Form.Group as={Col} className='mx-auto'>
+                    <InputGroup className='my-2'>
+                        <Form.Label column sm="2" className='subtitle mx-2 w-25'>
+                            Name
+                        </Form.Label>
+                        <Form.Control
+                            type="text"
+                            onChange={(e) => setName(e.target.value)}
+                            value={name}
+                        />
+                        <InputGroup.Text>
+                            <MdShoppingCart />
+                        </InputGroup.Text>
+                    </InputGroup>
+                    <InputGroup className='my-2'>
+                        <Form.Label column sm="2" className='subtitle mx-2 w-25'>
+                            Count
+                        </Form.Label>
+                        <Form.Control
+                            type="number"
+                            min={1}
+                            onKeyPress={event => preventCharacters(event)}
+                            onChange={(e) => setCount(e.target.value)}
+                            value={count}
+                        />
+                        <InputGroup.Text>
+                            <FaPlusMinus />
+                        </InputGroup.Text>
+                    </InputGroup>
+                    <InputGroup className='my-2'>
+                        <Form.Label column sm="2" className='subtitle mx-2 w-25'>
+                            Price
+                        </Form.Label>
+                        <Form.Control
+                            type="number"
+                            inputMode="decimal"
+                            min={1}
+                            onKeyPress={event => preventCharacters(event)}
+                            onChange={(e) => setPrice(e.target.value)}
+                            value={price}
+                        />
+                        <InputGroup.Text>
+                            <span>{getCurrencySymbol(country)}</span>
+                        </InputGroup.Text>
+                    </InputGroup>
+                    <InputGroup className='my-2'>
+                        <Form.Label column sm="2" className='subtitle mx-2 w-25'>
+                            Category
+                        </Form.Label>
+                        <Form.Select
+                            aria-label="gender"
+                            onChange={(e) => {
+                                setCategory(e.target.value);
+                            }}
+                            value={category}
+                        >
+                            {ProductCategories.map((category) => (
+                                <option key={category} value={category}>
+                                    {category}
+                                </option>
+                            ))}
+                        </Form.Select>
+                    </InputGroup>
+                </Form.Group>
+            </Modal.Body>
+            <Modal.Footer className='modal-styled-bg justify-content-around'>
+                <Button onClick={() => deleteProduct()}>Remove Product</Button>
+            </Modal.Footer>
+        </Modal>,
+        document.body
+    );
 };
+
 const mapStateToProps = (state) => ({
     list: state.items.list,
 });
+
 const mapDispatchToProps = {
     deleteProductFromList,
     updateProductsListRequest,
 };
+
 export default connect(mapStateToProps, mapDispatchToProps)(EditProductModal);
