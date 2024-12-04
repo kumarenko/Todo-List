65fe80e0ed831c9e108fb8bd044d7e10b992b1db
YuriiMarku <yurii.marku@infologiq.com>
2024-12-04 21:54:18 +0100

    Added script for production build

--- webpack.config.js
+++ webpack.config.js
@@ -1,66 +1,71 @@
 const path = require('path');
 const Dotenv = require('dotenv-webpack');
-
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin'); // Для минификации
 
-module.exports = {
-    mode: "development",
-    entry: ['./src/index.tsx'],
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: "[name].[hash].js",
-        publicPath: '/',  // Добавьте эту строку для корректной работы маршрутизации
-    },
-    devServer: {
-        port: 3001,
-        historyApiFallback: true,
-        // https: {
-        //     key: path.resolve(__dirname, 'server.key'),
-        //     cert: path.resolve(__dirname, 'server.cert'),
-        // },
-        // host: '192.168.55.4'
+module.exports = (env, argv) => {
+    const isProd = argv.mode === 'production'; // Проверка на продакшн
 
-    },
-    resolve: {
-        extensions: ['.jsx', '.tsx', '.js', '.ts', '.json']
-    },
-    plugins: [
-        new HTMLWebpackPlugin({
-            template: './src/index.html', // HTML-шаблон
-            favicon: './src/media/logo.ico', // Путь к файлу фавиконки
-        }),
-        new CleanWebpackPlugin(),
-        new Dotenv(),
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx|ts|tsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: [
-                            '@babel/preset-env',
-                            '@babel/preset-react',
-                            '@babel/preset-typescript'
-                        ]
+    return {
+        mode: isProd ? 'production' : 'development', // Задаем режим сборки
+        entry: ['./src/index.tsx'],
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: isProd ? '[name].[contenthash].js' : '[name].[hash].js', // Для продакшн используем contenthash для кэширования
+            publicPath: '/',
+        },
+        devServer: {
+            port: 3001,
+            historyApiFallback: true,
+        },
+        resolve: {
+            extensions: ['.jsx', '.tsx', '.js', '.ts', '.json']
+        },
+        plugins: [
+            new HTMLWebpackPlugin({
+                template: './src/index.html',
+                favicon: './src/media/logo.ico',
+            }),
+            new CleanWebpackPlugin(),
+            new Dotenv(),
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx|ts|tsx)$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: "babel-loader",
+                        options: {
+                            presets: [
+                                '@babel/preset-env',
+                                '@babel/preset-react',
+                                '@babel/preset-typescript'
+                            ]
+                        }
                     }
+                },
+                {
+                    test: /\.(png|jpg|jpeg|gif|svg|webp)$/,
+                    type: 'asset/resource',
+                    generator: {
+                        filename: 'images/[name][hash][ext][query]'
+                    }
+                },
+                {
+                    test: /\.(css|less)$/,
+                    use: ['style-loader', 'css-loader', 'less-loader']
                 }
-            },
-            {
-                test: /\.(png|jpg|jpeg|gif|svg|webp)$/,
-                type: 'asset/resource',  // Используем встроенный тип для обработки изображений
-                generator: {
-                    filename: 'images/[name][hash][ext][query]'  // Путь для выходных файлов
-                }
-            },
-            {
-                test: /\.(css|less)$/,
-                use: ['style-loader', 'css-loader', 'less-loader']
+            ]
+        },
+        optimization: {
+            minimize: isProd, // Минимизируем код только в продакшне
+            minimizer: [new TerserPlugin()],
+            splitChunks: {
+                chunks: 'all',
             }
-        ]
-    },
-    devtool: 'eval-source-map'
+        },
+        devtool: isProd ? 'source-map' : 'eval-source-map', // Для продакшн используем source-map для лучшей отладки
+    };
 };
